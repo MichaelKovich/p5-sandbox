161ccce3289fd20b832716ae27db868ed898671d
Eyobell Berhe <eyobell.berhe@live.longwood.edu>
2018-04-20 16:11:58 -0500

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
-
-import Homepage from './components/App';
+import React, { Component } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import BrownianMotion from "./animations/BrownianMotion";
+import Oscillator from "./animations/Oscillator";
+import Spirograph from "./animations/Spirograph";
+import App from "./App";
+import Reach from "./animations/Reach";
+import LSystems from "./animations/LSystems";
 
 export default (
   <Switch>
-    <Route component={Homepage} exact path="/" />
+    <Route exact path="/" component={App} />
+    <Route exact path="/BrownianMotion" component={BrownianMotion} />
+    <Route exact path="/Oscillator" component={Oscillator} />
+    <Route exact path="/Spirograph" component={Spirograph} />
+    <Route exact path="/Reach" component={Reach} />
+    <Route exact path="/LSystems" component={LSystems} />
   </Switch>
 );
